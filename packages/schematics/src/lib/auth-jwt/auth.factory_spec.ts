1eca9d0ab308e71c570789642e12bf3614902bd7
monica.lopez-gris <monica.lopez-gris@capgemini.com>
2020-09-07 10:44:19 +0200

    add auth-jwt tests

--- packages/schematics/src/lib/auth-jwt/auth.factory_spec.ts
+++ packages/schematics/src/lib/auth-jwt/auth.factory_spec.ts
@@ -1,15 +1,48 @@
-// import { Tree } from '@angular-devkit/schematics';
-// import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
-// import * as path from 'path';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
+import * as path from 'path';
 
-// const collectionPath = path.join(__dirname, '../collection.json');
-
-// describe('schematics', () => {
-//   it('works', () => {
-//     const runner = new SchematicTestRunner('schematics', collectionPath);
-//     const tree = runner.runSchematic('schematics', {}, Tree.empty());
-
-//     expect(tree.files).toEqual([]);
-//   });
-// });
-// //
+describe('Typeorm Factory', () => {
+  const runner: SchematicTestRunner = new SchematicTestRunner(
+    '.',
+    path.join(process.cwd(), 'src/collection.json'),
+  );
+  it('should set on module app', () => {
+    const app: object = {
+      name: 'path',
+    };
+    let tree: UnitTestTree = runner.runSchematic('application', app);
+    const options: object = {
+      path: 'path'
+    };
+    tree = runner.runSchematic('auth-jwt', options, tree);
+    const files: string[] = tree.files;
+    expect(
+      files.find(filename => filename === '/path/src/app/core/core.module.ts'),
+    ).toBeDefined();
+    expect(tree.readContent('/path/src/app/core/core.module.ts')).toEqual(
+      "import { Global, Module } from '@nestjs/common';\n" +
+      "import { ClassSerializerInterceptor } from '@devon4node/common/serializer';\n" +
+      "import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core';\n" +
+      "import { WinstonLogger } from '../shared/logger/winston.logger';\n" +
+      "import { BusinessLogicFilter } from '../shared/filters/business-logic.filter';\n" +
+      "import { AuthModule } from './auth/auth.module';\n" +
+      "import { UserModule } from './user/user.module';\n" +
+      "\n" +
+      "@Global()\n" +
+      "@Module({\n" +
+      "  imports: [UserModule, AuthModule],\n" +
+      "  controllers: [],\n" +
+      "  providers: [\n" +
+      "    { provide: APP_FILTER, useClass: BusinessLogicFilter },\n" +
+      "    { provide: APP_INTERCEPTOR, useClass: ClassSerializerInterceptor },\n" +
+      "    WinstonLogger,\n" +
+      "  ],\n" +
+      "  exports: [UserModule, AuthModule, WinstonLogger],\n" +
+      "})\n" +
+      "export class CoreModule {}\n"
+    );
+  });
+});
